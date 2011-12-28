e54adf20b3580070a2347268b2f4e5fcc86a7189
ajclark <napta2k@gmail.com>
2011-12-28 18:57:16 +0000

    fixed indentation. I think

--- public/js/iptables.js
+++ public/js/iptables.js
@@ -1,31 +1,31 @@
 /*
- * iptables.js - A seasonal holiday hack by Allan Clark
- * Fork me on github: 
- * http://github.com/ajclark/iptables.js
- *
- */
+* iptables.js - A seasonal holiday hack by Allan Clark
+* Fork me on github: 
+* http://github.com/ajclark/iptables.js
+*
+*/
 
 function generateIPTables() {
 	function setDefaultPolicy(chain) {
 		rulesetId.appendChild(document.createElement("p"));
 		rulesetId.appendChild(document.createTextNode("-P " + chain + " DROP"));
 	}
-	
+
 	function getRulesetId() {
 		return document.getElementById("ruleset");
 	}
-	
+
 	function evaluateCheckbox(checkboxName, outputText) {
 		if (document.form.elements[checkboxName].checked) {
-		    rulesetId.appendChild(document.createElement("p"));
-		    rulesetId.appendChild(document.createTextNode(outputText));
+			rulesetId.appendChild(document.createElement("p"));
+			rulesetId.appendChild(document.createTextNode(outputText));
 		}
 	}
-		
-    if (document.form.elements["isRedHat"].checked) {
-	    input = "RH-Firewall-1-INPUT";
-	    output = "RH-Firewall-1-OUTPUT";
-	    forward = "RH-Firewall-1-FORWARD";
+
+	if (document.form.elements["isRedHat"].checked) {
+		input = "RH-Firewall-1-INPUT";
+		output = "RH-Firewall-1-OUTPUT";
+		forward = "RH-Firewall-1-FORWARD";
 	}
 	else {
 		input = "INPUT";
@@ -33,17 +33,17 @@ function generateIPTables() {
 		forward = "FORWARD";
 	}
 
-    // Make way for the firewall rules
-    var rulesetId = getRulesetId();	
+	// Make way for the firewall rules
+	var rulesetId = getRulesetId();	
 	rulesetId.innerHTML="";
-	
+
 	// Set default Policies (DROP)
-    setDefaultPolicy(input);
-    setDefaultPolicy(output);
-    setDefaultPolicy(forward);
+	setDefaultPolicy(input);
+	setDefaultPolicy(output);
+	setDefaultPolicy(forward);
 
 	cidr = document.form.elements["network"].value;
-		
+
 	evaluateCheckbox("loopback", "-A " + input + " -i lo -j ACCEPT");
 	evaluateCheckbox("outgoing", "-I " + output + " 1 -m state --state RELATED,ESTABLISHED -j ACCEPT");
 	evaluateCheckbox("outgoing", "-A " + output + " -p udp --dport 53 -m state --state NEW -j ACCEPT");
@@ -58,6 +58,6 @@ function generateIPTables() {
 	evaluateCheckbox("udp_53", "-A " + input + " -p udp -m udp --dport 53 -s " + cidr + " -j ACCEPT");
 	evaluateCheckbox("icmp_8", " -A " + input + " -p icmp --icmp-type 8 -s " + cidr + " -j ACCEPT");
 	evaluateCheckbox("icmp_11", "-A " + input + " -p icmp --icmp-type 11 -s " + cidr + " -j ACCEPT");
-	
+
 	return false;
 }
