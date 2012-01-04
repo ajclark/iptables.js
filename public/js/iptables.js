bec7a35643f8631cd7db3b274ed7ed74daf85055
ajclark <napta2k@gmail.com>
2012-01-04 22:34:35 +0000

    Added custom TCP port range feature

--- public/js/iptables.js
+++ public/js/iptables.js
@@ -44,10 +44,13 @@ function generateIPTables() {
 
 	cidr = document.form.elements["network"].value;
 
+    // Loopback and outbound state rules
 	evaluateCheckbox("loopback", "-A " + input + " -i lo -j ACCEPT");
 	evaluateCheckbox("outgoing", "-I " + output + " 1 -m state --state RELATED,ESTABLISHED -j ACCEPT");
 	evaluateCheckbox("outgoing", "-A " + output + " -p udp --dport 53 -m state --state NEW -j ACCEPT");
 	evaluateCheckbox("outgoing", "-A " + output + " -o lo -j ACCEPT");
+	
+	// TCP and UDP port rules
 	evaluateCheckbox("tcp_21", "-A " + input + " -p tcp --dport 21 -m state --state NEW -s " + cidr + " -j ACCEPT");
 	evaluateCheckbox("tcp_22", "-A " + input + " -p tcp --dport 22 -m state --state NEW -s " + cidr + " -j ACCEPT");
 	evaluateCheckbox("tcp_25", "-A " + input + " -p tcp --dport 25 -m state --state NEW -s " + cidr + " -j ACCEPT");
@@ -56,6 +59,23 @@ function generateIPTables() {
 	evaluateCheckbox("udp_123", "-A " + input + " -p udp -m udp --dport 123 -s " + cidr + " -j ACCEPT");
 	evaluateCheckbox("udp_67", "-A " + input + " -p udp -m udp --dport 67 -s " + cidr + " -j ACCEPT");
 	evaluateCheckbox("udp_53", "-A " + input + " -p udp -m udp --dport 53 -s " + cidr + " -j ACCEPT");
+	
+	// Arbitrary TCP port range rules
+	start_port = document.form.elements["start_port"].value;
+	end_port = document.form.elements["end_port"].value;
+	
+	if ((start_port) && (end_port)) {
+		for (port=start_port; port<=end_port; port++) {
+			rulesetId.appendChild(document.createElement("p"));
+			rulesetId.appendChild(document.createTextNode("-A " + input + " -p tcp --dport " + port + " -m state --state NEW -s " + cidr + " -j ACCEPT"));	
+		}
+	}
+	else if ((start_port) && (!end_port)) {
+		rulesetId.appendChild(document.createElement("p"));
+		rulesetId.appendChild(document.createTextNode("-A " + input + " -p tcp --dport " + start_port + " -m state --state NEW -s " + cidr + " -j ACCEPT"));
+	}	
+	
+	// ICMP rules
 	evaluateCheckbox("icmp_8", " -A " + input + " -p icmp --icmp-type 8 -s " + cidr + " -j ACCEPT");
 	evaluateCheckbox("icmp_11", "-A " + input + " -p icmp --icmp-type 11 -s " + cidr + " -j ACCEPT");
 
